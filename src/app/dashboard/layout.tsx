e4d2075af789086d2ee48f14a603f305688dc472
khangeepk@example.com <khangeepk@example.com>
2026-03-11 21:04:28 +0500

    debug: add Header back to test for crash

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import Header from '@/components/Header'
+
 export default function DashboardLayout({
     children,
 }: {
@@ -6,6 +8,7 @@ export default function DashboardLayout({
     return (
         <div className="flex h-screen bg-[#F1F5F9] overflow-hidden">
             <div className="flex flex-col flex-1 overflow-hidden relative">
+                <Header />
                 <main className="flex-1 overflow-y-auto p-4 md:p-8">
                     {children}
                 </main>
